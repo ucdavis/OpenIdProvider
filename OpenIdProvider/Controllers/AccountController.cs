c641a34dc3406960af131a3348425546d67dcb8f
Scott Kirkland <srkirkland@gmail.com>
2012-05-22 09:34:45 -0700

    Added account controller with redirects to cas

---
+++ OpenIdProvider/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using UCDArch.Web.Authentication;
+
+namespace OpenIdProvider.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult LogOn(string returnUrl)
+        {
+            string resultUrl = CASHelper.Login(); //Do the CAS Login
+
+            if (resultUrl != null)
+            {
+                return Redirect(resultUrl);
+            }
+
+            TempData["URL"] = returnUrl;
+
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("https://cas.ucdavis.edu/cas/logout");
+        }
+    }
+}
--- OpenIdProvider/Controllers/HomeController.cs
+++ OpenIdProvider/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UCDArch.Core.Utils;
 
 namespace OpenIdProvider.Controllers
 {
