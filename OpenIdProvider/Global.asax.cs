5e5ea1f8a5c08abe8192c6e2a2d495f0b1ab952d
Scott Kirkland <srkirkland@gmail.com>
2012-05-22 08:43:10 -0700

    added in dotnetopen auth packages

--- OpenIdProvider/Global.asax.cs
+++ OpenIdProvider/Global.asax.cs
@@ -12,6 +12,8 @@ namespace OpenIdProvider
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static readonly object BehaviorInitializationSyncObject = new object();
+
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
@@ -35,6 +37,24 @@ namespace OpenIdProvider
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
+        }
+
+        private static void InitializeBehaviors()
+        {
+            //TODO: do we need anon identifier providers?
+            /*
+            if (DotNetOpenAuth.OpenId.Provider.Behaviors.PpidGeneration.PpidIdentifierProvider == null)
+            {
+                lock (BehaviorInitializationSyncObject)
+                {
+                    if (DotNetOpenAuth.OpenId.Provider.Behaviors.PpidGeneration.PpidIdentifierProvider == null)
+                    {
+                        DotNetOpenAuth.OpenId.Provider.Behaviors.PpidGeneration.PpidIdentifierProvider = new Code.AnonymousIdentifierProvider();
+                        DotNetOpenAuth.OpenId.Provider.Behaviors.GsaIcamProfile.PpidIdentifierProvider = new Code.AnonymousIdentifierProvider();
+                    }
+                }
+            }
+             */
         }
     }
 }
