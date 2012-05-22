336fd1d208f5b0b07c1bc15c5e0f94e55f219452
Scott Kirkland <srkirkland@gmail.com>
2012-05-22 09:22:43 -0700

    added xrds which responds to openId requests

--- OpenIdProvider/Controllers/HomeController.cs
+++ OpenIdProvider/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,15 @@ namespace OpenIdProvider.Controllers
     {
         public ActionResult Index()
         {
+            Debug.Assert(Request != null, "Request != null");
+            Debug.Assert(Request.AcceptTypes != null, "Request.AcceptTypes != null");
+
+            if (Request.AcceptTypes.Contains("application/xrds+xml"))
+            {
+                ViewData["OPIdentifier"] = true; //return xrds if this is an openId request
+                return View("Xrds");
+            }
+            
             ViewBag.Message = "Welcome to ASP.NET MVC!";
 
             return View();
